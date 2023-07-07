0254df1dc220bc4a1969fb6240663b1630980917
Priya Ranjan <97494807+RanjanPriya12@users.noreply.github.com>
2023-07-07 16:32:58 +0530

    post route to create a post

--- routes/post.route.js
+++ routes/post.route.js
@@ -4,7 +4,7 @@ const { isPostValidate } = require("../middlewares/postValidation");
 const { createPost, getAllPost, getSinglePost, updatePost, deletePost } = require("../controllers/post.controller");
 const router = express.Router();
 
-router.route("/addPost").post(isPostValidate, createPost);
+router.route("/addPost").post(isPostValidate,isAuthenticate, createPost);
 router.route("/:id").get(isAuthenticate, getSinglePost);
 router.route("/allPosts").get(isAuthenticate, getAllPost);
 router.route("/update-post/:id").put(isAuthenticate, updatePost);
